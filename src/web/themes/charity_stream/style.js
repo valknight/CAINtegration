15d2cace2873f194fc70f7fcf3e0d48b1317244f
Vallerie Knight <val@valknight.xyz>
2022-03-12 17:17:46 +0000

    add new theme for VKniLive's charity stream!

---
+++ src/web/themes/charity_stream/style.js
@@ -0,0 +1,4 @@
+// This script is loaded when basic.css is loaded as your theme
+// Put whatever modifications you might need in code here!
+
+console.log("hello from basic.js! (your current theme file)")
